8d6eeb625b5bc46b3572fd0feec314527f57dba7
KultzDev <84952418+kultxz@users.noreply.github.com>
2026-04-17 20:55:33 -0300

    Add files via upload

--- output/buttons.zig
+++ output/buttons.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-03 04:54:02.506970800 UTC
+// 2026-04-17 23:51:32.130851200 UTC
 
 pub const cs2_dumper = struct {
     // Module: client.dll
